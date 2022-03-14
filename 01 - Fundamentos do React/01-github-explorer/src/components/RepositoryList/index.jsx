0ec2dd71ab3ccc2d56c8228968bb2f446bd0dc5b
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-03-14 12:26:08 -0300

    terminando de configurar o babel e iniciando o componente

---
+++ 01 - Fundamentos do React/01-github-explorer/src/components/RepositoryList/index.jsx
@@ -0,0 +1,27 @@
+export function RepositoryList(){
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+
+      <ul>
+        <li>
+          <strong>Unform</strong>
+          <p>Form in react js</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+
+        <li>
+          <strong>Unform</strong>
+          <p>Form in react js</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+
+        <li>
+          <strong>Unform</strong>
+          <p>Form in react js</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
---
+++ 01 - Fundamentos do React/01-github-explorer/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const reactRefleshWebPackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+const modeExec = isDevelopment ? "development" : "production";
+
+const modeSourceMap = isDevelopment ? "eval-source-map" : "source-map"; // ou source-map
+
+const pluginsList = [];
+
+const plug = [];
+
+if (isDevelopment) {
+  pluginsList.push(new reactRefleshWebPackPlugin());
+  plug.push(require.resolve("react-refresh/babel"));
+}
+
+pluginsList.push(
+  new htmlWebpackPlugin({
+    template: path.resolve(__dirname, "public", "index.html"),
+  })
+);
+
+module.exports = {
+  mode: modeExec, // development || production
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: pluginsList,
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: plug,
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    // contentBase: path.resolve(__dirname, "public"),
+    hot: true,
+    port: 9000,
+    compress: false,
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    client: {
+      overlay: false,
+    },
+    //server: "https",
+    //host: "0.0.0.0",
+  },
+  performance: {
+    hints: false,
+  },
+  devtool: modeSourceMap,
+};
